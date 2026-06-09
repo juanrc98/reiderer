f19304f504c994844069cba8cad5d0bd5971f0db
Juan Rodriguez <juanrodcas98@gmail.com>
2026-06-09 13:09:26 +0200

    Cambio de dominio

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build
 export default defineConfig({
   // Cambia esto por tu dominio cuando lo tengas (p. ej. https://reiderer.com)
-  site: 'https://reiderer.com',
+  site: 'https://reiderer.vercel.app',
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
